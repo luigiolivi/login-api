29b261764cac1a104d38ebd6983cf380d555bce4
luigiolivi <eterowiczluigi@gmail.com>
2023-09-05 22:51:56 -0300

    express

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+class App {
+    constructor() {
+        this.app = express()
+
+        this.middlewares()
+        this.routes()
+    }
+    
+    middlewares() {
+        this.app.use(express.json())
+    }
+
+    routes() {
+
+    }
+}
