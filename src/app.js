bd5b390546cc06f563379bbc25070c66b0198d87
luigiolivi <eterowiczluigi@gmail.com>
2023-09-06 23:20:45 -0300

    Prettier config

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
-import express from 'express'
-import routes from './routes'
+import express from "express"
+import routes from "./routes"
 
 class App {
-    constructor() {
-        this.app = express()
+  constructor() {
+    this.app = express()
 
-        this.middlewares()
-        this.routes()
-    }
-    
-    middlewares() {
-        this.app.use(express.json())
-    }
+    this.middlewares()
+    this.routes()
+  }
 
-    routes() {
-        this.app.use(routes)
-    }
+  middlewares() {
+    this.app.use(express.json())
+  }
+
+  routes() {
+    this.app.use(routes)
+  }
 }
 
 export default new App().app
