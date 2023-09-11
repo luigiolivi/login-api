cf3b2db4b7ec2a1d9ff0036cf04410385a45cb03
luigiolivi <eterowiczluigi@gmail.com>
2023-09-11 20:36:14 -0300

    migration error fixed and database connection testing

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from "express"
+import { v4 } from "uuid"
+
+import User from "./app/models/User"
 
 const routes = new Router()
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Hello World!" })
+routes.get("/", async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: "Luigi",
+    email: "eterowiczluigii@gmail.com",
+    password_hash: "123321",
+  })
+  return response.json(user)
 })
 
 export default routes
